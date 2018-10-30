0c83e3ae9aa6b511c71a38c4dbcf48d77e1e6142
Josh Rondestvedt <atticus183@gmail.com>
2018-10-29 22:22:07 -0500

    all three VCs setup and working

--- SearchNoBackend/SearchBarInNavBarVC.swift
+++ SearchNoBackend/SearchBarInNavBarVC.swift
@@ -9,22 +9,106 @@
 import UIKit
 
 class SearchBarInNavBarVC: UIViewController {
+    
+    
+    let cellID = "cell2"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var filteredTodos = [Todo]()
+    var isFiltering = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        setupSearchInNavBar()
     }
     
+    //Search Setup
+    let searchController = UISearchController(searchResultsController: nil)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupSearchInNavBar() {
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.showsCancelButton = true
+        searchController.searchBar.delegate = self
+        definesPresentationContext = true
     }
-    */
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String) {
+        isFiltering = true
+        filteredTodos = searchText.isEmpty ? todos : todos.filter({ todo -> Bool in
+            return todo.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+    
+    func isFilteringFunc() -> Bool {
+        isFiltering = true
+        return searchController.isActive && !searchBarIsEmpty()
+    }
 
+}
+
+extension SearchBarInNavBarVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isFiltering ? filteredTodos.count : todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellID) else { return UITableViewCell() }
+        
+        let todo = isFiltering ? filteredTodos[indexPath.row] : todos[indexPath.row]
+        
+        cell.textLabel?.text = todo.name
+        cell.accessoryType = todo.isDone ? .checkmark : .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let todo = todos[indexPath.row]
+        
+        todo.isDone.toggle()
+        
+        tableView.reloadData()
+    }
+    
+    
+}
+
+extension SearchBarInNavBarVC: UISearchBarDelegate {
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        isFiltering = true
+//        filteredTodos = searchText.isEmpty ? todos : todos.filter({ todo -> Bool in
+//            return todo.name.lowercased().contains(searchText.lowercased())
+//        })
+//
+//        tableView.reloadData()
+//    }
+//
+////    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+////        searchBar.text = nil
+////        searchBar.endEditing(true)
+////
+////        isFiltering = false
+////        tableView.reloadData()
+////    }
+}
+
+extension SearchBarInNavBarVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
--- SearchNoBackend/SearchBarToNewScreen.swift
+++ SearchNoBackend/SearchBarToNewScreen.swift
@@ -9,22 +9,90 @@
 import UIKit
 
 class SearchBarToNewScreen: UIViewController {
+    
+    let cellID = "cell3"
+    
+    @IBOutlet weak var searchIcon: UIBarButtonItem!
+    @IBAction func searchIconAction(_ sender: UIBarButtonItem) {
+        isFiltering.toggle()
+        setupSearchBar()
+    }
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    var filteredTodos = [Todo]()
+    var isFiltering = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self
+        
+        if isFiltering {
+            setupSearchBar()
+        }
+        
+    }
+    
+    func setupSearchBar() {
+        let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        navigationController?.navigationBar.prefersLargeTitles = false
+        searchBar.showsCancelButton = true
+        searchBar.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SearchBarToNewScreen: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isFiltering ? filteredTodos.count : todos.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableview.dequeueReusableCell(withIdentifier: cellID) else { return UITableViewCell() }
+        
+        let todo = isFiltering ? filteredTodos[indexPath.row] : todos[indexPath.row]
+        
+        cell.textLabel?.text = todo.name
+        cell.accessoryType = todo.isDone ? .checkmark : .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let todo = todos[indexPath.row]
+        
+        todo.isDone.toggle()
+        
+        tableview.reloadData()
+    }
+    
+    
+}
 
+extension SearchBarToNewScreen: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        isFiltering = true
+        filteredTodos = searchText.isEmpty ? todos : todos.filter({ todo -> Bool in
+            return todo.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableview.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = nil
+        searchBar.endEditing(true)
+        navigationItem.titleView = nil
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        isFiltering = false
+        tableview.reloadData()
+        
+    }
 }
